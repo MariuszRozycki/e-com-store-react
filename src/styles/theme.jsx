107ef2e13626c5fcc954b86071bbb355a1985f89
MariuszRozycki <mariansc83@icloud.com>
2024-10-03 11:57:41 +0200

    Switch off types in eslint.config.js

---
+++ src/styles/theme.jsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  color: {
+    primary: "purple",
+    secondary: "red",
+  },
+};
+
+export const Theme = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+
+export default Theme;
