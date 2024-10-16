cd0c87d09bbcf23adc4b55216e67846b8ec5f8cb
MariuszRozycki <mariansc83@icloud.com>
2024-10-16 19:41:08 +0200

    Add functionality to button on Success

--- src/pages/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess.jsx
@@ -1,6 +1,14 @@
+import { useContext, useEffect } from "react";
 import { ContainerOutletContent, Success } from "../components";
+import { CartContext } from "../contexts/CartContext";
 
 const CheckoutSuccess = () => {
+  const { dispatch } = useContext(CartContext);
+
+  useEffect(() => {
+    dispatch({ type: "clearCart" });
+  }, [dispatch]);
+
   return (
     <div className='outlet-content'>
       <ContainerOutletContent>
